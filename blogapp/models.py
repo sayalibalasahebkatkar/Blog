9836990febf7dbe43350576b077bb94b2c67f643
sayali <sayalibk2002@gmail.com>
2022-03-22 19:21:30 +0530

    signin signup page

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    sno=models.AutoField(primary_key=True)
+    title=models.CharField(max_length=100)
+    content=models.CharField(max_length=10000)
+    author=models.CharField(max_length=30)
+    slug=models.CharField(max_length=130,default='')
+    timeStamp=models.DateTimeField(blank=True)
+
+    def __str__(self) -> str:
+        return self.title
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,9 +1,16 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+from blogapp.models import Blog
 
 # Create your views here.
 def bloghome(request):
-    return HttpResponse('this is blogsite')
+    allpost=Blog.objects.all()
+    context={'allpost':allpost}
+    # print(allpost[0].title)
+    return render(request,'blog/bloghome.html',context)
 
 def blogpost(request,slug):
-    return HttpResponse(f'BlogPost: {slug}')
+    post=Blog.objects.filter(slug=slug).first()
+    context={'post':post}
+    return render(request,'blog/blogpost.html',context)
